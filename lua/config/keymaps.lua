3aa6910ca77d8caa2edf2dbec722f91f74a404b0
ImKarma <imkarma.dev@gmail.com>
2024-11-08 19:21:00 +0200

    add fzf to buffer <leader>sf

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>sf", function()
+  -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+  builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+    winblend = 10,
+    previewer = false,
+  }))
+end, { desc = "Fuzzily search in current buffer" })
